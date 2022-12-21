b291f53d5abc49d0798ed53a5fe58f9b5ea4bfcb
milasantacruz <milalovechaton@gmail.com>
2022-12-21 11:20:44 -0300

    add json server ready

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function(){
     console.log("develop")
   }
   //GET PROYECTOS
-  var apiProyectos = 'http://localhost:5000/proyectos'
+  var apiProyectos = 'https://my-json-server.typicode.com/milasantacruz/miniback-jsonServer/proyectos'
   var getProyectos = async () =>{
    var laborContainer = $("#laboral");
   try{
@@ -103,7 +103,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
  //GET EDUCACION
 
- var apiEducacion = 'http://localhost:5000/educacion'
+ var apiEducacion = 'https://my-json-server.typicode.com/milasantacruz/miniback-jsonServer/educacion'
  var getEducacion = async () =>{
   var educContainer = $("#educacion");
  try{
